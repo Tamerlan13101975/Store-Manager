9f5f532bebe4b25a539c3dbc54630b2409eb58b1
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-03 19:02:36 +0300

    creating items

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ class Store:
         self.name = name
         self.address = address
         self.items = {}
+
+    def add_item(self, item, price):
+        self.items[item] = price
+
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
+
+    def get_price(self, item):
+        return self.items.get(item, None)
+
+    def update_price(self, item, new_price):
+        if item in self.items:
+            self.items[item] = new_price
